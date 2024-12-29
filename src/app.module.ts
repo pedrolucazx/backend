f8f93d920ce758a830209bbff6ba2dbfb9d4a008
pedrolucazx <pedrolucastrainee@gmail.com>
2024-12-29 10:07:29 -0300

    feat: add postgreSQL docker-compose and typeORM settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT),
+      password: process.env.DB_PASSWORD,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
