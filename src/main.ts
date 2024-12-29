91d375d1f9ef38bca2ae7585fb7df9f2b9f632fa
pedrolucazx <pedrolucastrainee@gmail.com>
2024-12-29 08:47:26 -0300

    feat(swagger): add swagger to document endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('User Management and Authentication API')
+    .setDescription(
+      'API para gerenciamento de usuários e autenticação, incluindo operações de criação, edição, exclusão e listagem de usuários.',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
